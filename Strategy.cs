540562ebf8125c2e74397025d3350e2aee5edb15
MENNAN SEVİM <mennansevim@gmail.com>
2020-05-14 09:07:30 +0300

    Update Strategy.cs

--- Strategy.cs
+++ Strategy.cs
@@ -3,6 +3,6 @@ namespace StategyDesignPattern
     // Strategy > Transfer işlemlerimiz bu arayüz üzerinden gerçekleşecek.
     interface ITransfer
     {
-        void DoTransfer();
+        void Send();
     }
 }
